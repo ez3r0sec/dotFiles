199f8aba8aa5915f3ca4d8a4b4b6aec37a3a3a18
Chips <34905191+ez3r0sec@users.noreply.github.com>
2018-07-27 11:05:53 -0400

    Add files via upload

---
+++ mkBashScript.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# mkBashScript.sh
+# set up a bash script using a standard format
+# Last Edited: 7/27/18 Julian Thies
+
+if [ -z "$1" ] ; then
+	echo "No script name passed in"
+	exit
+else
+	echo "Building framework for script $1 in $(pwd)"
+fi
+
+echo "#!/bin/bash" > "$1"
+echo "# $1" >> "$1"
+echo "# " >> "$1"
+echo "$ Last Edited: $(date +%m/%d/%y)" >> "$1"
+echo >> "$1"
+echo "### VARIABLES" >> "$1"
+echo >> "$1"
+echo >> "$1"
+echo "### FUNCTIONS" >> "$1"
+echo >> "$1"
+echo >> "$1"
+echo "### SCRIPT" >> "$1"
+echo >> "$1"
+echo >> "$1"
+
+echo "Script initialized"
